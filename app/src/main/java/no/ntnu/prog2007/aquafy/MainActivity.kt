b71f89fed0ac1b517ab76902fe657ffeb2189381
Sigurd Riseth <sigurd.riseth@gmail.com>
2025-10-15 15:36:40 +0200

    Fix MainActivity theme usage and edge-to-edge setup

--- app/src/main/java/no/ntnu/prog2007/aquafy/MainActivity.kt
+++ app/src/main/java/no/ntnu/prog2007/aquafy/MainActivity.kt
@@ -1,2 +1,23 @@
 package no.ntnu.prog2007.aquafy
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import no.ntnu.prog2007.aquafy.ui.theme.AppTheme
+import no.ntnu.prog2007.aquafy.ui.AquafyApp
+
+/**
+ * Loads the home screen of the Aquafy application.
+ */
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppTheme {
+                AquafyApp()
+            }
+        }
+    }
+}
